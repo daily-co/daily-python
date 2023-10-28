12f0d225caaff39935258234c90eb1a71be6455b
Jack Hoge <me@jackhoge.com>
2023-10-28 13:08:51 -0400

    YOLO at 10fps

--- demos/yolo/yolo.py
+++ demos/yolo/yolo.py
@@ -1,8 +1,11 @@
 import argparse
 import os
-from ultralytics import YOLO
+import torch
+import io
 import time
 from threading import Thread
+from PIL import Image
+import io
 
 from daily import EventHandler, CallClient, Daily
 from datetime import datetime
@@ -14,8 +17,9 @@ class DailyYOLO(EventHandler):
         ):
 
         self.url = url
-        self.model = YOLO('yolov5s.pt')
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
         self.configure_daily()
+        self.time = time.time()
 
         while True:
             time.sleep(1)
@@ -23,7 +27,7 @@ class DailyYOLO(EventHandler):
     def configure_daily(self):
         Daily.init()
         self.client = CallClient(event_handler = self)
-        self.camera = Daily.create_camera_device("camera", width = 1280, height = 720, color_format = "BGRA")
+        self.camera = Daily.create_camera_device("camera", width = 1280, height = 720, color_format="RGB")
         self.client.join(self.url)
         self.camera_started = False
 
@@ -39,19 +43,19 @@ class DailyYOLO(EventHandler):
     def on_participant_joined(self, participant):
         print(f"on_participant_joined: {participant}")
 
-        if not participant["info"]["userName"]:
-            return
-
         self.client.set_video_renderer(participant["id"],
-                                         self.on_video_frame,
-                                         color_format = "BGRA")
+                                         self.on_video_frame)
         
 
     def on_video_frame(self, participant_id, video_frame):
-        self.camera.write_frame(video_frame.buffer)
+        if time.time() - self.time > 0.1:
+            self.time = time.time()
+            image = Image.frombytes("RGBA", (video_frame.width, video_frame.height), video_frame.buffer)
+            result = self.model(image)
 
-        result = self.model(video_frame.buffer)
-        result.print()
+            pil = Image.fromarray(result.render()[0], mode="RGB").tobytes()
+
+            self.camera.write_frame(pil)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="YOLO exmaple with Daily")
