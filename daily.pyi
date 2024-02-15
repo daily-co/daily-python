6d71c957d58a9b1709ace15b2665cf21f1b3afb2
Aleix Conchillo Flaqué <aleix@daily.co>
2024-02-15 11:37:07 -0800

    daily-python: fix daily.pyi formatting

--- daily.pyi
+++ daily.pyi
@@ -8,6 +8,7 @@
 
 from typing import Any, Callable, Mapping, Optional, Sequence
 
+
 class Daily:
 
     @staticmethod
@@ -19,24 +20,27 @@ class Daily:
         ...
 
     @staticmethod
-    def create_camera_device(device_name: str,
-                             width: int,
-                             height: int,
-                             color_format: str = "RGBA") -> VirtualCameraDevice:
+    def create_camera_device(
+            device_name: str,
+            width: int,
+            height: int,
+            color_format: str = "RGBA") -> VirtualCameraDevice:
         ...
 
     @staticmethod
-    def create_speaker_device(device_name: str,
-                              sample_rate: int = 16000,
-                              channels: int = 1,
-                              non_blocking: bool = False) -> VirtualSpeakerDevice:
+    def create_speaker_device(
+            device_name: str,
+            sample_rate: int = 16000,
+            channels: int = 1,
+            non_blocking: bool = False) -> VirtualSpeakerDevice:
         ...
 
     @staticmethod
-    def create_microphone_device(device_name: str,
-                                 sample_rate: int = 16000,
-                                 channels: int = 1,
-                                 non_blocking: bool = False) -> VirtualMicrophoneDevice:
+    def create_microphone_device(
+            device_name: str,
+            sample_rate: int = 16000,
+            channels: int = 1,
+            non_blocking: bool = False) -> VirtualMicrophoneDevice:
         ...
 
     @staticmethod
@@ -52,11 +56,16 @@ class CallClient:
     def join(self,
              meeting_url: str,
              meeting_token: Optional[str] = None,
-             client_settings: Optional[Mapping[str, Any]] = None,
-             completion: Optional[Callable[[Optional[Mapping[str, Any]], Optional[str]], None]] = None) -> None:
+             client_settings: Optional[Mapping[str,
+                                               Any]] = None,
+             completion: Optional[Callable[[Optional[Mapping[str,
+                                                             Any]],
+                                            Optional[str]],
+                                           None]] = None) -> None:
         ...
 
-    def leave(self, completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+    def leave(self, completion: Optional[Callable[[
+              None, Optional[str]], None]] = None) -> None:
         ...
 
     def set_user_name(self, user_name: str) -> None:
@@ -72,91 +81,119 @@ class CallClient:
         ...
 
     def update_remote_participants(self,
-                                   remote_participants: Mapping[str, Any],
-                                   completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+                                   remote_participants: Mapping[str,
+                                                                Any],
+                                   completion: Optional[Callable[[None,
+                                                                  Optional[str]],
+                                                                 None]] = None) -> None:
         ...
 
     def eject_remote_participants(self,
                                   ids: Sequence[str],
-                                  completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+                                  completion: Optional[Callable[[None,
+                                                                 Optional[str]],
+                                                                None]] = None) -> None:
         ...
 
     def inputs(self) -> Mapping[str, Any]:
         ...
 
-    def update_inputs(self,
-                      input_settings: Mapping[str, Any],
-                      completion: Optional[Callable[[Optional[Mapping[str, Any]], Optional[str]], None]] = None) -> None:
+    def update_inputs(self, input_settings: Mapping[str, Any], completion: Optional[Callable[[
+                      Optional[Mapping[str, Any]], Optional[str]], None]] = None) -> None:
         ...
 
     def publishing(self) -> Mapping[str, Any]:
         ...
 
-    def update_publishing(self,
-                          publishing_settings: Mapping[str, Any],
-                          completion: Optional[Callable[[Optional[Mapping[str, Any]], Optional[str]], None]] = None) -> None:
+    def update_publishing(self, publishing_settings: Mapping[str, Any], completion: Optional[Callable[[
+                          Optional[Mapping[str, Any]], Optional[str]], None]] = None) -> None:
         ...
 
     def subscriptions(self) -> Mapping[str, Any]:
         ...
 
     def update_subscriptions(self,
-                             participant_settings: Optional[Mapping[str, Any]] = None,
-                             profile_settings: Optional[Mapping[str, Any]] = None,
-                             completion: Optional[Callable[[Optional[Mapping[str, Any]], Optional[str]], None]] = None) -> None:
+                             participant_settings: Optional[Mapping[str,
+                                                                    Any]] = None,
+                             profile_settings: Optional[Mapping[str,
+                                                                Any]] = None,
+                             completion: Optional[Callable[[Optional[Mapping[str,
+                                                                             Any]],
+                                                            Optional[str]],
+                                                           None]] = None) -> None:
         ...
 
     def subscription_profiles(self) -> Mapping[str, Any]:
         ...
 
-    def update_subscription_profiles(self,
-                                     profile_settings: Mapping[str, Any],
-                                     completion: Optional[Callable[[Optional[Mapping[str, Any]], Optional[str]], None]] = None) -> None:
+    def update_subscription_profiles(self, profile_settings: Mapping[str, Any], completion: Optional[Callable[[
+                                     Optional[Mapping[str, Any]], Optional[str]], None]] = None) -> None:
         ...
 
     def update_permissions(self,
-                           permissions: Mapping[str, Any],
-                           completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+                           permissions: Mapping[str,
+                                                Any],
+                           completion: Optional[Callable[[None,
+                                                          Optional[str]],
+                                                         None]] = None) -> None:
         ...
 
     def start_recording(self,
-                        streaming_settings: Optional[Mapping[str, Any]] = None,
+                        streaming_settings: Optional[Mapping[str,
+                                                             Any]] = None,
                         stream_id: Optional[str] = None,
                         force_new: Optional[bool] = None,
-                        completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+                        completion: Optional[Callable[[None,
+                                                       Optional[str]],
+                                                      None]] = None) -> None:
         ...
 
     def stop_recording(self,
                        stream_id: Optional[str] = None,
-                       completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+                       completion: Optional[Callable[[None,
+                                                      Optional[str]],
+                                                     None]] = None) -> None:
         ...
 
     def update_recording(self,
-                         update_settings: Optional[Mapping[str, Any]] = None,
+                         update_settings: Optional[Mapping[str,
+                                                           Any]] = None,
                          stream_id: Optional[str] = None,
-                         completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+                         completion: Optional[Callable[[None,
+                                                        Optional[str]],
+                                                       None]] = None) -> None:
         ...
 
     def start_transcription(self,
-                            settings: Optional[Mapping[str, Any]] = None,
-                            completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+                            settings: Optional[Mapping[str,
+                                                       Any]] = None,
+                            completion: Optional[Callable[[None,
+                                                           Optional[str]],
+                                                          None]] = None) -> None:
         ...
 
-    def stop_transcription(self, completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+    def stop_transcription(self, completion: Optional[Callable[[
+                           None, Optional[str]], None]] = None) -> None:
         ...
 
     def start_dialout(self,
-                      settings: Optional[Mapping[str, Any]] = None,
-                      completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+                      settings: Optional[Mapping[str,
+                                                 Any]] = None,
+                      completion: Optional[Callable[[None,
+                                                     Optional[str]],
+                                                    None]] = None) -> None:
         ...
 
-    def stop_dialout(self, completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+    def stop_dialout(self, completion: Optional[Callable[[
+                     None, Optional[str]], None]] = None) -> None:
         ...
 
     def send_app_message(self,
                          message: Any,
                          participant: Optional[str] = None,
-                         completion: Optional[Callable[[None, Optional[str]], None]] = None) -> None:
+                         completion: Optional[Callable[[None,
+                                                        Optional[str]],
+                                                       None]] = None) -> None:
         ...
 
     def get_network_stats(self) -> Mapping[str, Any]:
@@ -175,12 +212,14 @@ class CallClient:
                            color_format: str = "RGBA") -> None:
         ...
 
+
 class EventHandler:
 
     def __init__(self) -> None:
         ...
 
-    def on_active_speaker_changed(self, participant: Mapping[str, Any]) -> None:
+    def on_active_speaker_changed(
+            self, participant: Mapping[str, Any]) -> None:
         ...
 
     def on_app_message(self,
@@ -188,8 +227,8 @@ class EventHandler:
                        sender: Mapping[str, Any]) -> None:
         ...
 
-    def on_available_devices_updated(self,
-                                     available_devices: Mapping[str, Any]) -> None:
+    def on_available_devices_updated(
+            self, available_devices: Mapping[str, Any]) -> None:
         ...
 
     def on_call_state_updated(self, state: Mapping[str, Any]) -> None:
@@ -234,7 +273,8 @@ class EventHandler:
     def on_participant_updated(self, participant: Mapping[str, Any]) -> None:
         ...
 
-    def on_publishing_updated(self, publishing_settings: Mapping[str, Any]) -> None:
+    def on_publishing_updated(
+            self, publishing_settings: Mapping[str, Any]) -> None:
         ...
 
     def on_recording_error(self,
@@ -248,11 +288,12 @@ class EventHandler:
     def on_recording_stopped(self, stream_id: Mapping[str, Any]) -> None:
         ...
 
-    def on_subscription_profiles_updated(self,
-                                         subscription_profiles: Mapping[str, Any]) -> None:
+    def on_subscription_profiles_updated(
+            self, subscription_profiles: Mapping[str, Any]) -> None:
         ...
 
-    def on_subscriptions_updated(self, subscriptions: Mapping[str, Any]) -> None:
+    def on_subscriptions_updated(
+            self, subscriptions: Mapping[str, Any]) -> None:
         ...
 
     def on_transcription_error(self, message: Mapping[str, Any]) -> None:
@@ -268,6 +309,7 @@ class EventHandler:
                                  stopped_by: Mapping[str, Any],
                                  stopped_by_error: Mapping[str, Any]) -> None:
         ...
+
 
 class AudioData:
 
@@ -314,6 +356,7 @@ class VideoFrame:
     def color_format(self) -> str:
         ...
 
+
 class VirtualCameraDevice:
 
     @property
@@ -332,8 +375,12 @@ class VirtualCameraDevice:
     def color_format(self) -> str:
         ...
 
-    def write_frame(self, frame: bytes, completion: Optional[Callable[[bytes], None]] = None) -> None:
+    def write_frame(self,
+                    frame: bytes,
+                    completion: Optional[Callable[[bytes],
+                                                  None]] = None) -> None:
         ...
+
 
 class VirtualMicrophoneDevice:
 
@@ -352,6 +399,7 @@ class VirtualMicrophoneDevice:
     def write_frames(self, frame: bytes) -> None:
         ...
 
+
 class VirtualSpeakerDevice:
 
     @property
@@ -366,5 +414,8 @@ class VirtualSpeakerDevice:
     def channels(self) -> int:
         ...
 
-    def read_frames(self, num_frame: int, completion: Optional[Callable[[bytes], None]] = None) -> bytes:
+    def read_frames(self,
+                    num_frame: int,
+                    completion: Optional[Callable[[bytes],
+                                                  None]] = None) -> bytes:
         ...
