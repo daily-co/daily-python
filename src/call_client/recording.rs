0b4a2d130611a335c9f0746e702dd9859dea185c
Aleix Conchillo Flaqué <aleix@daily.co>
2026-06-04 14:30:39 -0700

    CSDK-2945 daily-python: dont serialize StartRecordingProperties null properties
    Fixes #46

--- src/call_client/recording.rs
+++ src/call_client/recording.rs
@@ -4,7 +4,10 @@ use serde_json::Value;
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct StartRecordingProperties {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub streaming_settings: Option<Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub instance_id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub force_new: Option<bool>,
 }
